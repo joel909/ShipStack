545947639c45155c7876e77f9dc9c4588849d369
Joel Joby <joeljobyp@gmail.com>
2025-04-18 23:16:20 +0530

    folder upload compleated

--- api/app.py
+++ api/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, jsonify, request,render_template
 app = Flask(__name__)
-
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+@app.route('/api/upload/flask', methods=['POST'])
+def upload_file():
+    print(request.files)
+    return jsonify({'filename': "df"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
